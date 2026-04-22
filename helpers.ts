b9e481a4a5c7d8e75719b097e5dfadf9f8969c60
rafi1m <159406589+rafi1m@users.noreply.github.com>
2026-04-23 00:45:23 +0600

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:44:24.201Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-22T18:45:38.454Z
+Commit: Patch API endpoint error handling
