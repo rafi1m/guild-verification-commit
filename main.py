1358dac850f281878aec32f229485ef2bb3edd33
rafi1m <159406589+rafi1m@users.noreply.github.com>
2026-04-23 00:44:54 +0600

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:45:01.334Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-04-22T18:45:09.641Z
+Commit: Fix CSS styling bug on mobile devices
