304ac3f8804305dccd4a102a3f3d7d431cbe9cc0
rafi1m <159406589+rafi1m@users.noreply.github.com>
2026-04-23 00:41:54 +0600

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T18:40:59.048Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-22T18:42:08.846Z
+Commit: Fix CSS styling bug on mobile devices
